5e09ec971ac8a8b6a675f2199c40194c1ac4cd50
KG-BT GEB H FAMILY <joelg.hossi29.jlgh@gmail.com>
2024-12-27 20:50:37 +0100

    My app banking

---
+++ types/indexd.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */ 
+
+declare type SearchParamProps = {
+    params: { [key: string]: string }; 
+    searchParams: { [key: string]: string | string[] | 
+        undefined }; 
+    };
+
+
+    // =====================================
+
+declare type SingUpParams = {
+    firstName: string;
+    lasttName: string;
+    adress1: string;
+    city: string;
+    state: string;
+    postalcode: string;
+    dateOfBirth: string;
+    ssn: string;
+    email: string;
+    password: string;
+    
+}
+
